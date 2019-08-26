e9627cd7bf79d6f08596d29c187bbc51a15d193e
CHUNGHYEONOH <48762809+CHUNGHYEONOH@users.noreply.github.com>
2019-08-26 23:27:29 +0900

    musinsa clothes crawling sample

--- Web-Crawling/crawling.py
+++ Web-Crawling/crawling.py
@@ -2,6 +2,20 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("https://store.musinsa.com/app/contents/bestranking?u_cat_cd=001")
-bsObject = BeautifulSoup(html, "html.parser")
+page = BeautifulSoup(html, "html.parser")
 
-print(bsObject)
+ul = page.find("ul", {"class":"snap-article-list boxed-article-list article-list center list goods_small_media8"})
+
+li = ul.find_all("li", {"class":"li_box"})
+
+top_name = []
+
+
+
+for l in li:
+    div = l.find("div", {"class":"li_inner"})
+    div = div.find("div", {"class":"article_info"})
+    p = div.find("p", {"class":"item_title"})
+    a = p.find("a").text
+    print(str(a))
+
