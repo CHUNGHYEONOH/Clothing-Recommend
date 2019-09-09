48b6e543f9109b7956617503dfaf51c0f867a50b
CHUNGHYEONOH <48762809+CHUNGHYEONOH@users.noreply.github.com>
2019-09-09 23:14:02 +0900

    crawling and DB update

--- Web-Crawling/crawling.py
+++ Web-Crawling/crawling.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import MySQLdb
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 #MYSQL connection
 connect = MySQLdb.connect(port = 1902, host='localhost', user='root', password='1234', database='mysql', charset='utf8')
@@ -25,7 +28,24 @@ ul = page.find("ul", {"class":"snap-article-list boxed-article-list article-list
 
 li = ul.find_all("li", {"class":"li_box"})
 
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://store.musinsa.com/app/contents/bestranking?u_cat_cd=001')
+
+# Enter next Page
+pagenext = driver.find_element_by_css_selector("a.fa.fa-angle-right.paging-btn.btn.next")
+pagenext.send_keys(Keys.ENTER)
+pagenext.send_keys(Keys.ENTER)
+pagenext.send_keys(Keys.ENTER)
+print(driver.current_url)
+
+
+'''
 for l in li:
+    rank = l.find("div", {"class":"icon_best"})
+    rank = rank.text
+    rank = re.findall('[0-9]', rank)
+    rank = ''.join(rank)        #순위
+
     div = l.find("div", {"class":"li_inner"})
     div = div.find("div", {"class":"list_img"})
     a = div.find("a", {"class":"img-block"})
@@ -44,15 +64,19 @@ for l in li:
     image = currentpage.find("div",{"class":"product-img"})
     image = image.find("img")
     image = image['src']        #이미지
-    price = 5000
+    price = currentpage.find("span", {"class":"product_article_price"})
+    price = price.text        #가격
+    price = re.findall('[0-9]', price)
+    price = ''.join(price)
     DATA = "INSERT INTO clothes VALUES (" + "'" + name + "'" + ", " + "'" + brand\
-        + "'" + ", " + "'" + str(price) + "'" + ", " + "'" + image + "'" +  ", " + "'" + category + "'" + ", " + "'" + subcategory + "'" + ")"
+        + "'" + ", " + "'" + price + "'" + ", " + "'" + image + "'" +  ", "\
+        + "'" + category + "'" + ", " + "'" + subcategory + "'" + ")"
     try:
         cursor.execute(DATA)
     except:
         print(name+brand+category)
     # 이름, 브랜드, 가격, 이미지, 카테고리, 세부카테고리
-
+'''
 '''
     table = currentpage.find("table", {"class":"table_th_grey"}).find("tbody")
     table = table.find_all("tr")    
